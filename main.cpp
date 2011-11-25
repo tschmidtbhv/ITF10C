117549083d26829ade9ad750add8f24860b3a1cf
Christian Ziegenrücker <splatterb0y@hotmail.de>
2011-11-25 10:42:45 +0100

    Spacekiller

--- main.cpp
+++ main.cpp
@@ -31,41 +31,41 @@ int main(void) {
     getNetClassDigets();
     createSubnets();
     showSubnetMenu();
-    
-    return EXIT_SUCCESS;    
-} 
+
+    return EXIT_SUCCESS;
+}
 
 
 void getNetClassDigets() {
     string input = "";
-    
+
     do {
         cout << "Möchten Sie eine andere Netzklasse nutzen als 192.168.0.X ? (Y/N) " << endl; 
         cout << "> ";
-        getline(cin,input); 
+        getline(cin,input);
         // Eingabe in Kleinschreibung umwandeln
         transform(input.begin(),input.end(),input.begin(),::tolower);
-        
+
     } while(input != "n" && input != "y");
-    
+
     if(input == "y") {
-        
+
         makeIpBlocks(255);
-        
+
     } else {
         ipBlock1 = 192;
         ipBlock2 = 168;
         ipBlock3 = 0;
     }
-    
+
     ipBlock4 = 0;
-    
+
     return;
 }
 
 void makeIpBlocks(long max_broadcast) {
     /* Jeden Einzelnen Block nach extra Parametern abfragen, die zwischen 1 und 255 liegen */
-    
+
     do {
         cout << "Block 1: " << endl;
         cout << "> ";
@@ -75,7 +75,7 @@ void makeIpBlocks(long max_broadcast) {
             cout << "Bitte geben Sie eine Zahl zwischen 0 und " << max_broadcast << " ein." << endl;
         }
     } while(ipBlock1 < 0 || ipBlock1 > max_broadcast );
-    
+
     do {
         cout << "Block 2: " << endl;
         cout << "> ";
@@ -85,7 +85,7 @@ void makeIpBlocks(long max_broadcast) {
             cout << "Bitte geben Sie eine Zahl zwischen 0 und " << max_broadcast << " ein." << endl;
         }
     } while(ipBlock2 < 0 || ipBlock2 > max_broadcast );
-    
+
     do {
         cout << "Block 3: " << endl;
         cout << "> ";
@@ -95,14 +95,14 @@ void makeIpBlocks(long max_broadcast) {
             cout << "Bitte geben Sie eine Zahl zwischen 0 und " << max_broadcast << " ein." << endl;
         }
     } while(ipBlock3 < 0 || ipBlock3 > max_broadcast );
-    
+
     cout << endl;
 }
 
 void createSubnets() {
     cout << "Wieviele Subnetze möchten Sie anlegen? " << endl;
     cout << "> ";
-    
+
     int networkCount = 0;
     do {
         scanf ("%d",&networkCount);
@@ -115,7 +115,7 @@ void createSubnets() {
 
     for(int i = 0; i < networkCount; i++) {
         cout << "Subnetz " << (i+1) << ":" << endl;
-        
+
         string name = "";
         string description = "";
         int networkSize = 0;
@@ -126,12 +126,11 @@ void createSubnets() {
             cout << "> ";
             getline(cin,name);
         } while(name == "");
-        
-        
+
         cout << "Beschreibung? " << endl;
         cout << "> ";
         getline(cin,description);
-                
+
         do {
             cout << "Größe? " << endl;
             cout << "> ";
@@ -140,7 +139,7 @@ void createSubnets() {
             if(networkSize <= 0 || networkSize >= 256) {
                 cout << "Diese Größe ist nicht zulässig, wählen Sie eine Größe zwischen 0 und 256." << endl;
             }
-            
+
             usedSize = networkSize;
             for(int i = 0; i < usedSubnets.size(); i++) {
                 usedSize += usedSubnets[i].getSize();
@@ -153,25 +152,24 @@ void createSubnets() {
         } while(networkSize <= 0 || networkSize >= 256);
         cout << endl;
 
-        
         //Das erste Subnetz wird erstellt
         if (i == 0) {
             /*
              Abfragen ob es wirklich ein 192.168.0. Netz ist,
              gibt ja auch 172.16.XXX.XXX für VPNs
-             
+
               192.168.0.0/24, das /24 muss berücksichtigt werden.
-              In der offiziellen Aufgabe ist es AFAIK ein /25 
+              In der offiziellen Aufgabe ist es AFAIK ein /25
             */
-            
+
             stringstream ipStream;
             ipStream << ipBlock1 << ".";
             ipStream << ipBlock2 << ".";
             ipStream << ipBlock3 << ".";
             ipStream << ipBlock4;
-            
+
             string ipString = ipStream.str();
-            
+
             usedSubnets.push_back(
                                   subnet(nextExpToTwo(networkSize),
                                          splitAndConvert(ipString),
@@ -182,16 +180,16 @@ void createSubnets() {
                                     description)
                                 );
         } else {
-            
+
             /* @Thomas, bitte nochmal drüber schauen ob das hier alles so Sinn macht */
-            
+
             long max_broadcast = 0;
             for (int i = 0; i< usedSubnets.size(); i++) {
                 if( usedSubnets[i].getBroadCast() > max_broadcast) {
                     max_broadcast = usedSubnets[i].getBroadCast();
                 }
             }
-            
+
             usedSubnets.push_back(
                                   subnet(nextExpToTwo(networkSize),
                                     max_broadcast+1,
@@ -201,7 +199,7 @@ void createSubnets() {
                                     name,
                                     description)
                                 );
-            
+
         }
     }
 }
@@ -209,11 +207,11 @@ void createSubnets() {
 void showSubnetMenu() {
     int menu_input = 0;
     int max_value = 0;
-    
+
     do {
         out->showSubnet(usedSubnets);
         max_value = (int)usedSubnets.size();
-    
+
         cout << "0 - Beenden" << endl;
         cout << "Welches Subnetz möchten Sie bearbeiten? Bitte geben Sie die Zahl ein. " << endl;
         cout << "Benutzen Sie -1 um nach IP Adressen zu suchen." << endl;
@@ -221,9 +219,9 @@ void showSubnetMenu() {
         cout << "> ";
         scanf("%d",&menu_input);
         cout << endl;
-        
+
         if (menu_input == -1) {
-        
+
             string ipAddress = "";
             cout << "Geben Sie die zu suchende IP Adresse ein" << endl;
             cout << "> ";
@@ -231,7 +229,7 @@ void showSubnetMenu() {
             getline(cin, ipAddress);
             searchForIp(splitAndConvert(ipAddress));
             break;
-        
+
         } else if(menu_input == 0) {
             break;
         }
@@ -241,22 +239,21 @@ void showSubnetMenu() {
             cout << "Bitte erneut eingeben." << endl;
         }
     } while(menu_input != 0);
-    
+
     return;
 }
 
 
 /*
- * editSubnet 
+ * editSubnet
  * Edit name or Description
  * @params int menu_input
  * return void
  *
  */
 void editSubnet(int menu_input){
-    
+
     int eingabe;
-    
     do {
         cout << "Sie bearbeiten Subnetz: " << usedSubnets[menu_input].getName() << endl;
         cout << "Beschreibung: " << usedSubnets[menu_input].getNotice() << endl;
@@ -270,31 +267,30 @@ void editSubnet(int menu_input){
         cout << "4 - IP mit Hostnamen und Beschreibung hinzufügen" << endl;
         cout << "0 - Bearbeitung beenden" << endl;
         cout << "> ";
-        
+
         scanf("%d",&eingabe);
         switch (eingabe) {
-            
+
             case 1: {
                 string name = "";
                 cout << "Geben Sie einen neuen Namen ein:" << endl;
                 cout << "> ";
                 cin.ignore();
-                getline(cin, name); 
+                getline(cin, name);
                 usedSubnets[menu_input].setName(name);
                 break;
             }
-                
+
             case 2: {
                 string description = "";
                 cout << "Geben Sie eine neue Beschreibung ein:" << endl;
                 cout << "> ";
                 cin.ignore();
-                getline(cin, description); 
+                getline(cin, description);
                 usedSubnets[menu_input].setNotice(description);
                 break;
             }
-            
-            
+
             case 3: {
                 string ipAddress = "";
                 cout << "Geben Sie die zu suchende IP Adresse ein" << endl;
@@ -304,7 +300,7 @@ void editSubnet(int menu_input){
                 searchForIp(splitAndConvert(ipAddress));
                 break;
             }
-                
+
             case 4: {
                 addIPToSubnet(menu_input);
                 break;
@@ -316,7 +312,7 @@ void editSubnet(int menu_input){
         }
         cout << endl;
     } while (eingabe >= 1 && eingabe <= 3);
-    
+
     return;
 }
 
@@ -326,21 +322,21 @@ void editSubnet(int menu_input){
  * @params int menu_input
  * return void
  *
- */ 
+ */
 
 void addIPToSubnet(int menu_input) {
-    
+
     long max_broadcast = 0;
     bool value = false;
     vector<ipaddress> usedAdresses;
     max_broadcast = usedSubnets[menu_input].getBroadCast();
-    
+
     cout << "Wie lautet der letzte Block der IP?" << endl;
     cout << "> ";
     scanf("%d",&ipBlock4);
-    
+
     ipaddress address = ipaddress::ipaddress(ipBlock1,ipBlock2,ipBlock3,ipBlock4);
-    
+
     if(usedSubnets[menu_input].getRangeStart() <= address.getLong() &&
        address.getLong() <= usedSubnets[menu_input].getRangeEnd()
        )
@@ -353,7 +349,7 @@ void addIPToSubnet(int menu_input) {
         }
         if(value == false) {
             cout << "Ip-Adresse wird hinzugefügt." << endl;
-            usedSubnets[menu_input].addIP(address); 
+            usedSubnets[menu_input].addIP(address);
         } else {
             cout << "IP-Adresse bereits vorhanden." << endl;
         }
@@ -369,33 +365,32 @@ void addIPToSubnet(int menu_input) {
  * edit ip in subnet
  * @params int menu_input
  * return void
- *  
  */
 
 void editIPInSubnet(int menu_input) {
-    
+
     vector<ipaddress> addresses = usedSubnets[menu_input].getAddresses();
     cout << "ip te: " << addresses[1].getString();
     for (int i = 0; i < addresses.size(); i++) {
         cout << "ip: " << addresses[i].getString();
     }
-    
+
     return;
 }
 
 /**
  * splitAndConvert
  * Split the input value and convert char string to int
- * 
+ *
  * @return void
  */
 long splitAndConvert(string paramIPAddress) {
-    
+
     long output = 0;
     int iteration = 0;
     char* IPAddress =  strdup(paramIPAddress.c_str());
     char* pch = strtok(IPAddress, ".");
-    
+
     while(pch != NULL) {
         switch(iteration) {
             case 0:
@@ -414,24 +409,24 @@ long splitAndConvert(string paramIPAddress) {
         pch = strtok (NULL, ".");
         iteration++;
     }
-    
+
     return output;
 }
 
 /**
- * Nächst größere Zahl auf Basis der zwei Bestimmen 
+ * Nächst größere Zahl auf Basis der zwei Bestimmen
  *
  * @param Zahl zu der die größere Zahl bestimmt werden soll
  * @return Zahl größer als input, auf Basis von zwei
  */
 int nextExpToTwo(int input) {
-    
+
     int calculate = 0;
-    
+
     for(int i = 0; calculate <= input; i++) {
         calculate = pow(2,i);
     }
-    
+
     return calculate;
 }
 
